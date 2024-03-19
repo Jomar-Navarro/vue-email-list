18c254d902eb037566ef3ebdf498bac663390759
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-19 15:40:14 +0100

    [add] addOne Email

--- js/script.js
+++ js/script.js
@@ -7,16 +7,27 @@ createApp({
   data(){
 
     return {
-      title: 'Email list'
+      title: 'Email list',
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      addEmail: '',
     }
 
   },
 
   methods: {
+    getApi(){
+      axios.get(this.apiUrl)
+      .then ( (risposta) => {
+        console.log(this.addEmail = risposta.data.response);
+      })
 
+      .catch((error) => {
+        console.log();
+      })
+    }
   },
 
   mounted(){
-
+    this.getApi()
   },
 }).mount('#app')
