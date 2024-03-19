66169603314c71b00df042a01385903d824e2d29
Jomar-Navarro <Jomarnavarro201@gmail.com>
2024-03-19 15:28:47 +0100

    [add] first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+console.log('vue-email-list');
+
+const { createApp } = Vue;
+
+createApp({
+
+  data(){
+
+    return {
+      title: 'Email list'
+    }
+
+  },
+
+  methods: {
+
+  },
+
+  mounted(){
+
+  },
+}).mount('#app')
